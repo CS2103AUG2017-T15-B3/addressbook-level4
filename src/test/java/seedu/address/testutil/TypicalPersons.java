c7bd680686863845b017d7b9624b2d1c74145525
DarrenCzen <zen_darren@hotmail.com>
2017-10-11 23:10:25 +0800

    Updated JUnits4 Testing for most files

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -10,6 +10,8 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_WEBSITE_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_WEBSITE_BOB;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -24,36 +26,57 @@ import seedu.address.model.person.exceptions.DuplicatePersonException;
  */
 public class TypicalPersons {
 
-    public static final ReadOnlyPerson ALICE = new PersonBuilder().withName("Alice Pauline")
+    public static final ReadOnlyPerson ALICE = new PersonBuilder()
+            .withName("Alice Pauline")
             .withAddress("123, Jurong West Ave 6, #08-111").withEmail("alice@example.com")
-            .withPhone("85355255")
+            .withPhone("85355255").withWebsite("https://www.facebook.com/alice")
             .withTags("friends").build();
-    public static final ReadOnlyPerson BENSON = new PersonBuilder().withName("Benson Meier")
+    public static final ReadOnlyPerson BENSON = new PersonBuilder()
+            .withName("Benson Meier")
             .withAddress("311, Clementi Ave 2, #02-25")
             .withEmail("johnd@example.com").withPhone("98765432")
+            .withWebsite("https://www.facebook.com/benson")
             .withTags("owesMoney", "friends").build();
-    public static final ReadOnlyPerson CARL = new PersonBuilder().withName("Carl Kurz").withPhone("95352563")
-            .withEmail("heinz@example.com").withAddress("wall street").build();
-    public static final ReadOnlyPerson DANIEL = new PersonBuilder().withName("Daniel Meier").withPhone("87652533")
-            .withEmail("cornelia@example.com").withAddress("10th street").build();
-    public static final ReadOnlyPerson ELLE = new PersonBuilder().withName("Elle Meyer").withPhone("9482224")
-            .withEmail("werner@example.com").withAddress("michegan ave").build();
-    public static final ReadOnlyPerson FIONA = new PersonBuilder().withName("Fiona Kunz").withPhone("9482427")
-            .withEmail("lydia@example.com").withAddress("little tokyo").build();
-    public static final ReadOnlyPerson GEORGE = new PersonBuilder().withName("George Best").withPhone("9482442")
-            .withEmail("anna@example.com").withAddress("4th street").build();
+    public static final ReadOnlyPerson CARL = new PersonBuilder()
+            .withName("Carl Kurz").withPhone("95352563")
+            .withEmail("heinz@example.com").withAddress("wall street")
+            .withWebsite("https://www.facebook.com/carl").build();
+    public static final ReadOnlyPerson DANIEL = new PersonBuilder()
+            .withName("Daniel Meier").withPhone("87652533")
+            .withEmail("cornelia@example.com").withAddress("10th street")
+            .withWebsite("https://www.facebook.com/daniel").build();
+    public static final ReadOnlyPerson ELLE = new PersonBuilder()
+            .withName("Elle Meyer").withPhone("9482224")
+            .withEmail("werner@example.com").withAddress("michegan ave")
+            .withWebsite("https://www.facebook.com/elle").build();
+    public static final ReadOnlyPerson FIONA = new PersonBuilder()
+            .withName("Fiona Kunz").withPhone("9482427")
+            .withEmail("lydia@example.com").withAddress("little tokyo")
+            .withWebsite("https://www.facebook.com/fiona").build();
+    public static final ReadOnlyPerson GEORGE = new PersonBuilder()
+            .withName("George Best").withPhone("9482442")
+            .withEmail("anna@example.com").withAddress("4th street")
+            .withWebsite("https://www.facebook.com/george").build();
 
     // Manually added
-    public static final ReadOnlyPerson HOON = new PersonBuilder().withName("Hoon Meier").withPhone("8482424")
-            .withEmail("stefan@example.com").withAddress("little india").build();
-    public static final ReadOnlyPerson IDA = new PersonBuilder().withName("Ida Mueller").withPhone("8482131")
-            .withEmail("hans@example.com").withAddress("chicago ave").build();
+    public static final ReadOnlyPerson HOON = new PersonBuilder()
+            .withName("Hoon Meier").withPhone("8482424")
+            .withEmail("stefan@example.com").withAddress("little india")
+            .withWebsite("https://www.facebook.com/hoon").build();
+    public static final ReadOnlyPerson IDA = new PersonBuilder()
+            .withName("Ida Mueller").withPhone("8482131")
+            .withEmail("hans@example.com").withAddress("chicago ave")
+            .withWebsite("https://www.facebook.com/IDA").build();
 
     // Manually added - Person's details found in {@code CommandTestUtil}
-    public static final ReadOnlyPerson AMY = new PersonBuilder().withName(VALID_NAME_AMY).withPhone(VALID_PHONE_AMY)
-            .withEmail(VALID_EMAIL_AMY).withAddress(VALID_ADDRESS_AMY).withTags(VALID_TAG_FRIEND).build();
-    public static final ReadOnlyPerson BOB = new PersonBuilder().withName(VALID_NAME_BOB).withPhone(VALID_PHONE_BOB)
-            .withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB).withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND)
+    public static final ReadOnlyPerson AMY = new PersonBuilder()
+            .withName(VALID_NAME_AMY).withPhone(VALID_PHONE_AMY)
+            .withEmail(VALID_EMAIL_AMY).withAddress(VALID_ADDRESS_AMY)
+            .withWebsite(VALID_WEBSITE_AMY).withTags(VALID_TAG_FRIEND).build();
+    public static final ReadOnlyPerson BOB = new PersonBuilder()
+            .withName(VALID_NAME_BOB).withPhone(VALID_PHONE_BOB)
+            .withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB)
+            .withWebsite(VALID_WEBSITE_BOB).withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND)
             .build();
 
     public static final String KEYWORD_MATCHING_MEIER = "Meier"; // A keyword that matches MEIER
