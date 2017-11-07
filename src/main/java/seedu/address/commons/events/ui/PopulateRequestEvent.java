96688aec6bde0fe6f193d5ab1769552b73aa8f74
chernghann <chernghann.chia@gmail.com>
2017-11-07 12:25:55 +0800

    Change location for RequestEvent files

---
+++ src/main/java/seedu/address/commons/events/ui/PopulateRequestEvent.java
@@ -0,0 +1,24 @@
+package seedu.address.commons.events.ui;
+
+import seedu.address.commons.events.BaseEvent;
+import seedu.address.model.event.ReadOnlyEvent;
+import seedu.address.model.event.UniqueEventList;
+
+/**
+ * Indicates a request to add Event.
+ */
+public class PopulateRequestEvent extends BaseEvent {
+
+    public final ReadOnlyEvent event;
+    public final UniqueEventList eventList;
+
+    public PopulateRequestEvent(ReadOnlyEvent event, UniqueEventList eventList) {
+        this.event = event;
+        this.eventList = eventList;
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName();
+    }
+}
